c6d74c778466ad8732698ca067e505dd4f95fda2
Azaan Ali Raza <azaanaliraza@Azaans-Mac-Pro.local>
2024-09-14 22:00:16 +0530

    Did the first assignment

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ if (js === "123") alert("Js is amazing")
 
 console.log(10 + 40 + 60 + 33 + 22 + 43 - 11 - 45 - 78)
 
+//First assignment
+let country = "India"
+let continent = "Asia"
+let population = 200
+
+console.log(country)
+console.log(continent)
+console.log(population)
+//-------------------------------------------------------------
+
